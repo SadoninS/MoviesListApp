6ebc55c14585dda6f322016b6c0c690a77951ae0
SadoninS <stanislavsadonin@gmail.com>
2023-06-28 17:54:31 +0400

    add scrollbar to list

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const moviesListNode = document.querySelector('[data-moviesList]');
 renderApp();
 
 addBtnNode.addEventListener('click', addMovieHandler);
+inputNode.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) addMovieHandler();
+});
 moviesListNode.addEventListener('click', moviesListItemManipulationHandler);
