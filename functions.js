e5daee56c26946ebb1060cc12418b424b2c1a151
SadoninS <stanislavsadonin@gmail.com>
2023-07-03 16:30:09 +0400

    stage 2

--- functions.js
+++ functions.js
@@ -1,36 +1,36 @@
-function renderApp() {
-  moviesListNode.innerHTML = localStorage.getItem('moviesList');
-}
+// function renderApp() {
+//   moviesListNode.innerHTML = localStorage.getItem('moviesList');
+// }
 
-function saveToLS() {
-  localStorage.setItem('moviesList', moviesListNode.innerHTML);
-}
+// function saveToLS() {
+//   localStorage.setItem('moviesList', moviesListNode.innerHTML);
+// }
 
-function addMovieHandler() {
-  const inputText = inputNode.value.trim();
-  if (!inputText) alert('Введите название фильма!');
-  else {
-    const moviesListItem = document.createElement('li');
-    moviesListItem.classList.add('movies-list-item');
-    moviesListItem.innerHTML = inputText;
-    moviesListNode.appendChild(moviesListItem);
+// function addMovieHandler() {
+//   const inputText = inputNode.value.trim();
+//   if (!inputText) alert('Введите название фильма!');
+//   else {
+//     const moviesListItem = document.createElement('li');
+//     moviesListItem.classList.add('movies-list-item');
+//     moviesListItem.innerHTML = inputText;
+//     moviesListNode.appendChild(moviesListItem);
 
-    const delBtn = document.createElement('button');
-    delBtn.classList.add('del-btn');
-    moviesListItem.appendChild(delBtn);
-  }
-  inputNode.value = '';
-  inputNode.focus();
-  saveToLS();
-}
+//     const delBtn = document.createElement('button');
+//     delBtn.classList.add('del-btn');
+//     moviesListItem.appendChild(delBtn);
+//   }
+//   inputNode.value = '';
+//   inputNode.focus();
+//   saveToLS();
+// }
 
-function moviesListItemManipulationHandler(event) {
-  if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('checked');
-    saveToLS();
-  }
-  if (event.target.tagName === 'BUTTON') {
-    event.target.parentElement.remove();
-    saveToLS();
-  }
-}
+// function moviesListItemManipulationHandler(event) {
+//   if (event.target.tagName === 'LI') {
+//     event.target.classList.toggle('checked');
+//     saveToLS();
+//   }
+//   if (event.target.tagName === 'BUTTON') {
+//     event.target.parentElement.remove();
+//     saveToLS();
+//   }
+// }
--- index.js
+++ index.js
@@ -2,10 +2,66 @@ const inputNode = document.querySelector('[data-input]');
 const addBtnNode = document.querySelector('[data-add-btn]');
 const moviesListNode = document.querySelector('[data-moviesList]');
 
-renderApp();
+let movies = [];
 
-addBtnNode.addEventListener('click', addMovieHandler);
-inputNode.addEventListener('keypress', (event) => {
-  if (event.keyCode === 13) addMovieHandler();
+function setMovie() {
+  if (!inputNode.value) {
+    alert('Введите название фильма!');
+    return;
+  }
+  let movie = {
+    title: '',
+    id: 0,
+    checked: false,
+  };
+  movie.title = inputNode.value;
+  movie.id = Date.now();
+  movies.push(movie);
+  return movie;
+}
+
+function render(movies) {
+  moviesListNode.innerHTML = '';
+  movies.forEach((element) => {
+    const movieHTML = document.createElement('li');
+    movieHTML.classList.add('movies-list-item');
+    movieHTML.innerHTML = element.title;
+    movieHTML.setAttribute('id', `${element.id}`);
+    moviesListNode.appendChild(movieHTML);
+    const delBtn = document.createElement('button');
+    delBtn.classList.add('del-btn');
+    movieHTML.appendChild(delBtn);
+  });
+
+  inputNode.value = '';
+  inputNode.focus();
+}
+
+function moviesListItemManipulationHandler(event) {
+  movies.forEach((movie) => {
+    if (movie.id == event.target.id) {
+      event.target.classList.toggle('checked');
+      movie.checked = true;
+    }
+  });
+
+  if (event.target.className == 'del-btn') {
+    event.target.parentElement.remove();
+    let itemToDeledeIndex = movies.findIndex((item) => item.id == event.target.id);
+    movies.splice(itemToDeledeIndex, 1);
+  }
+}
+
+addBtnNode.addEventListener('click', function () {
+  setMovie();
+  render(movies);
 });
+
 moviesListNode.addEventListener('click', moviesListItemManipulationHandler);
+
+inputNode.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) {
+    setMovie();
+    render(movies);
+  }
+});
