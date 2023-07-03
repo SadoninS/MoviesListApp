ddc931a531ca34f3103f2e6359eed58174596150
SadoninS <stanislavsadonin@gmail.com>
2023-07-03 19:03:21 +0400

    ver1.1 chage render func: html -> objects

--- functions.js
+++ functions.js
@@ -1,36 +1,73 @@
-// function renderApp() {
-//   moviesListNode.innerHTML = localStorage.getItem('moviesList');
-// }
+function saveLS() {
+  LS = JSON.stringify(movies);
+  localStorage.setItem('moviesList', LS);
+}
 
-// function saveToLS() {
-//   localStorage.setItem('moviesList', moviesListNode.innerHTML);
-// }
+function setMovie() {
+  if (!inputNode.value) {
+    alert('Введите название фильма!');
+    return;
+  }
+  const movie = {
+    title: '',
+    id: 0,
+    checked: false,
+  };
+  movie.title = inputNode.value.trim();
+  movie.id = Date.now();
+  movies.push(movie);
+  saveLS();
+  return movie;
+}
 
-// function addMovieHandler() {
-//   const inputText = inputNode.value.trim();
-//   if (!inputText) alert('Введите название фильма!');
-//   else {
-//     const moviesListItem = document.createElement('li');
-//     moviesListItem.classList.add('movies-list-item');
-//     moviesListItem.innerHTML = inputText;
-//     moviesListNode.appendChild(moviesListItem);
+function render(movies) {
+  moviesListNode.innerHTML = '';
+  const moviesListHTML = document.createElement('ul');
+  moviesListHTML.classList.add('movies-list');
+  moviesListNode.appendChild(moviesListHTML);
+  movies.forEach((element) => {
+    const movieHTML = document.createElement('li');
+    movieHTML.classList.add('movies-list-item');
+    if (element.checked == true) movieHTML.classList.add('checked');
+    movieHTML.innerHTML = element.title;
+    movieHTML.setAttribute('id', `${element.id}`);
+    moviesListHTML.appendChild(movieHTML);
+    const delBtn = document.createElement('button');
+    delBtn.classList.add('del-btn');
+    movieHTML.appendChild(delBtn);
+  });
+  inputNode.value = '';
+  inputNode.focus();
+}
 
-//     const delBtn = document.createElement('button');
-//     delBtn.classList.add('del-btn');
-//     moviesListItem.appendChild(delBtn);
-//   }
-//   inputNode.value = '';
-//   inputNode.focus();
-//   saveToLS();
-// }
+function addMovieByClickHandler() {
+  setMovie();
+  render(movies);
+}
 
-// function moviesListItemManipulationHandler(event) {
-//   if (event.target.tagName === 'LI') {
-//     event.target.classList.toggle('checked');
-//     saveToLS();
-//   }
-//   if (event.target.tagName === 'BUTTON') {
-//     event.target.parentElement.remove();
-//     saveToLS();
-//   }
-// }
+function addMovieByEnterHandler() {
+  if (event.keyCode === 13) {
+    setMovie();
+    render(movies);
+  }
+}
+
+function moviesListItemManipulationHandler(event) {
+  movies.forEach((movie) => {
+    if (movie.id == event.target.id && movie.checked == true) {
+      event.target.classList.toggle('checked');
+      movie.checked = false;
+      saveLS();
+    } else if (movie.id == event.target.id && movie.checked == false) {
+      event.target.classList.toggle('checked');
+      movie.checked = true;
+      saveLS();
+    }
+  });
+  if (event.target.className == 'del-btn') {
+    const itemToDeleteIndex = movies.findIndex((movie) => event.target.parentElement.id == movie.id);
+    movies.splice(itemToDeleteIndex, 1);
+    event.target.parentElement.remove();
+    saveLS();
+  }
+}
--- index.js
+++ index.js
@@ -1,67 +1,10 @@
-const inputNode = document.querySelector('[data-input]');
+const moviesListNode = document.querySelector('[output-area]');
 const addBtnNode = document.querySelector('[data-add-btn]');
-const moviesListNode = document.querySelector('[data-moviesList]');
-
-let movies = [];
-
-function setMovie() {
-  if (!inputNode.value) {
-    alert('Введите название фильма!');
-    return;
-  }
-  let movie = {
-    title: '',
-    id: 0,
-    checked: false,
-  };
-  movie.title = inputNode.value;
-  movie.id = Date.now();
-  movies.push(movie);
-  return movie;
-}
-
-function render(movies) {
-  moviesListNode.innerHTML = '';
-  movies.forEach((element) => {
-    const movieHTML = document.createElement('li');
-    movieHTML.classList.add('movies-list-item');
-    movieHTML.innerHTML = element.title;
-    movieHTML.setAttribute('id', `${element.id}`);
-    moviesListNode.appendChild(movieHTML);
-    const delBtn = document.createElement('button');
-    delBtn.classList.add('del-btn');
-    movieHTML.appendChild(delBtn);
-  });
-
-  inputNode.value = '';
-  inputNode.focus();
-}
-
-function moviesListItemManipulationHandler(event) {
-  movies.forEach((movie) => {
-    if (movie.id == event.target.id) {
-      event.target.classList.toggle('checked');
-      movie.checked = true;
-    }
-  });
-
-  if (event.target.className == 'del-btn') {
-    event.target.parentElement.remove();
-    let itemToDeledeIndex = movies.findIndex((item) => item.id == event.target.id);
-    movies.splice(itemToDeledeIndex, 1);
-  }
-}
-
-addBtnNode.addEventListener('click', function () {
-  setMovie();
-  render(movies);
-});
+const inputNode = document.querySelector('[data-input]');
+let LS = JSON.parse(localStorage.getItem('moviesList'));
+let movies = Array.isArray(LS) ? LS : [];
+render(movies);
 
 moviesListNode.addEventListener('click', moviesListItemManipulationHandler);
-
-inputNode.addEventListener('keypress', (event) => {
-  if (event.keyCode === 13) {
-    setMovie();
-    render(movies);
-  }
-});
+addBtnNode.addEventListener('click', addMovieByClickHandler);
+inputNode.addEventListener('keypress', addMovieByEnterHandler);
